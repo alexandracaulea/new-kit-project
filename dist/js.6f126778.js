018e855ac3b1a69b10db314759f1fb66edc7047f
Alexandra Caulea <alexandra.caulea@gmail.com>
2020-12-04 07:18:49 +0200

    Add metadata

--- dist/js.6f126778.js
+++
@@ -1,8 +0,0 @@
-parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"TAds":[function(require,module,exports) {
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.byeWorld=exports.helloWorld=void 0;var e=function(){return"Hello World!"};exports.helloWorld=e;var r=function(){return"Bye World!"};exports.byeWorld=r;
-},{}],"s2Ev":[function(require,module,exports) {
-"use strict";var e=require("./utils");window.addEventListener("DOMContentLoaded",function(){var t=document.createElement("h1");t.textContent=(0,e.helloWorld)(),document.body.insertAdjacentElement("afterbegin",t)});
-},{"./utils":"TAds"}],"hgbR":[function(require,module,exports) {
-"use strict";require("./app");
-},{"./app":"s2Ev"}]},{},["hgbR"], null)
-//# sourceMappingURL=js.6f126778.js.map
